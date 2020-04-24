46a5ed42e8bb2aad6b5182a00d2fd5837d2d4059
s-aibar <s-aibar@example.com>
2020-04-24 15:00:12 +0200

    Transpose dotplot

--- page_tfsCellType_server.R
+++ page_tfsCellType_server.R
@@ -5,7 +5,7 @@ dotPlot.server <- function(input, output, session, filePath) {
   load(paste0(filePath))
   source("libs/dotheatmap.R")
   p <- dotheatmap(enrichmentDf=meanExprMat.df,
-                  var.x="cellType", var.y="gene",
+                  var.x="gene", var.y="cellType",
                   var.col="expression",
                   col.low="lightgrey", col.mid="floralwhite", col.high="red",
                   var.size="NES", min.size=.2, max.size=5)
