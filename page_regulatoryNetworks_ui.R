053bc3038a74c90d35c935e13e544e8c81cbfdd0
s-aibar <s-aibar@example.com>
2020-04-27 18:28:45 +0200

    Added markdown page for regulatory networks

--- page_regulatoryNetworks_ui.R
+++ page_regulatoryNetworks_ui.R
@@ -1,5 +1,3 @@
 page_regulatoryNetworks <- fluidPage(
-  h2("Cell-type specific regulatory networks"),
-  br(),
-  h4("Tables of enhancers, motifs etc.")
+  includeMarkdown("md/regulatoryNetworks.Rmd")
 )
