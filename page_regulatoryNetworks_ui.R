9cf9d85d28c4e535595f71b693f32d78f6459599
Dafni Papasokrati <dafni.papasokrati@kuleuven.vib.be>
2020-04-27 10:44:56 +0200

    network figures removed

--- page_regulatoryNetworks_ui.R
+++ page_regulatoryNetworks_ui.R
@@ -1,11 +1,5 @@
 page_regulatoryNetworks <- fluidPage(
   h2("Cell-type specific regulatory networks"),
   br(),
-  fluidRow(
-  column(width = 7,
-         img(src="img/networks1.png", width="90%")
-    ),
-    column(width = 5,
-          img(src="img/networks2.png", width="80%")
-    ))
+  h4("Tables of enhancers, motifs etc.")
 )
