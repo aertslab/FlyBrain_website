6a9fc6de2bb5284c95fa38387714806a2b4c9ad4
s-aibar <s-aibar@example.com>
2020-12-17 18:05:06 +0100

    update image path

--- page_enhancersDL.R
+++ page_enhancersDL.R
@@ -16,7 +16,7 @@ plot_EnhancerDL.ui <- function(id){
       # imageOutput(NS(id, "dlPlot"), height="100px", inline=T))# "10%")  # Does not resize... -.-
       uiOutput(NS(id, "deepExplainerPlot")),
       uiOutput(NS(id, "fullSizeLink"))
-    ))
+    )) # %>% withSpinner(color="#0dc5c1")
   )
 }
 
@@ -26,26 +26,29 @@ plot_EnhancerDL.server <- function(input, output, session, imgRootPath) {
   # Load: 
   enhFiles <- list.files(file.path(imgRootPath, 'deepExplainer/thumbnails'), pattern=".png")
   enhFiles <- unique(gsub("_th.png", "", enhFiles))
-  names(enhFiles) <- enhFiles
-  enhFiles <- setNames(enhFiles, sapply(strsplit(enhFiles, "___"), function(x) paste0(x[[2]], " (", x[[1]], ")")))
-  names(enhFiles) <- sapply(strsplit(names(enhFiles), "__"), function(x) x[[2]])
   
-  updateSelectInput(session, "enhID", choices=names(enhFiles), selected=names(enhFiles)[1])
+  enhFiles <- data.frame(fileName=enhFiles, stringsAsFactors=F)
+  enhFiles$cellType <- sapply(strsplit(enhFiles$fileName, "___"), function(x) x[[1]])
+  enhFiles$displayName <- sapply(strsplit(enhFiles$fileName, "___"), function(x) paste0(x[[2]], " (", x[[1]], ")"))
+  enhFiles$displayName <- sapply(strsplit(enhFiles$displayName, "__"), function(x) x[[2]])
+  rownames(enhFiles) <- enhFiles$displayName
+  
+  updateSelectInput(session, "enhID", choices=enhFiles$displayName, selected=enhFiles$displayName)
   
   # Start traking events (plots to show):
   observe({
-    enhSelected <- enhFiles[input$enhID]
+    enhSelected <- input$enhID 
     print(enhSelected)
     
     if(!is.na(enhSelected)){
       # output$dlPlot <- renderImage(list(src = imgPath,contentType = "image/png", alt="Enhancer view"), delete=FALSE) 
       output$deepExplainerPlot <- renderUI({
-        tagList(img(src=normalizePath(file.path('deepExplainer/thumbnails', paste(enhSelected, '_th.png', sep=''))), width="100%"))
+        tagList(img(src=file.path(file.path('deepExplainer/thumbnails', paste(enhFiles[enhSelected, "fileName"], '_th.png', sep=''))), width="100%"))
       })
       
       output$fullSizeLink <- renderUI({
         tagList(a("[View full size]", 
-                  href=normalizePath(file.path('deepExplainer', paste(enhSelected, '.png', sep=''))), 
+                  href=file.path(file.path('deepExplainer', enhFiles[enhSelected, "cellType"], paste(enhFiles[enhSelected, "fileName"], '.png', sep=''))), 
                   target="_blank"))
       })
     }
